7baf6582f4cb3f7d1bdd54b85fb29cdec878ca15
theapache64-charlie <theapache64@gmail.com>
2017-12-18 20:45:01 +0530

    Added button click listener support

--- src/ButterLayoutEngineServlet.java
+++ src/ButterLayoutEngineServlet.java
@@ -15,7 +15,9 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by theapache64 on 1/11/17.
@@ -25,6 +27,10 @@ public class ButterLayoutEngineServlet extends HttpServlet {
 
     private static final String KEY_XML_DATA = "xml_data";
     private static final String KEY_R_SERIES = "r_series";
+
+    private static String getFirstCharUppercase(String input) {
+        return input.substring(0, 1).toUpperCase() + input.substring(1);
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -54,7 +60,8 @@ public class ButterLayoutEngineServlet extends HttpServlet {
 
             NodeList nodeList = doc.getElementsByTagName("*");
 
-            StringBuilder butterLayoutBuilder = null;
+            StringBuilder codeBuilder = new StringBuilder();
+            final List<String> buttons = new ArrayList<>();
 
             for (int i = 0; i < nodeList.getLength(); i++) {
 
@@ -70,38 +77,59 @@ public class ButterLayoutEngineServlet extends HttpServlet {
 
                     // do something with the current element
                     final Node idNode = node.getAttributes().getNamedItem("android:id");
+
                     if (idNode != null) {
 
-                        if (butterLayoutBuilder == null) {
-                            butterLayoutBuilder = new StringBuilder("//Generated with ButterLayout (http://github.com/theapache64/butterLayout): " + new Date() + "\n");
+                        if (codeBuilder.length() == 0) {
+                            codeBuilder = new StringBuilder("//Generated with ButterLayout (http://github.com/theapache64/butterLayout): " + new Date() + "\n");
                         }
 
                         final String id = idNode.getNodeValue().split("/")[1];
-                        butterLayoutBuilder.append(String.format("@BindView(%s.id.%s)\n", rSeries, id));
+                        codeBuilder.append(String.format("@BindView(%s.id.%s)\n", rSeries, id));
 
                         if (nodeName.contains(".")) {
                             final String[] nodeNameChunks = nodeName.split("\\.");
                             nodeName = nodeNameChunks[nodeNameChunks.length - 1];
                         }
-                        butterLayoutBuilder.append(String.format("%s %s;\n\n", nodeName, id));
 
+                        codeBuilder.append(String.format("%s %s;\n\n", nodeName, id));
+
+                        if (nodeName.endsWith("Button")) {
+                            buttons.add(id);
+                        }
                     }
-
                 }
+
+            }
+
+            for (final String buttonId : buttons) {
+
+                codeBuilder
+                        .append(String.format("\n\n@OnClick(%s.id.%s)\n", rSeries, buttonId))
+                        .append(String.format("public void on%sClicked(){\n\n}", getFromFirstUpperChar(buttonId)));
             }
 
 
-            if (butterLayoutBuilder == null) {
+            if (codeBuilder.length() == 0) {
                 throw new RequestException("No view with id found");
             }
 
-            resp.getWriter().write(new Response("OK", "butter_layout", butterLayoutBuilder.toString()).getResponse());
+
+            resp.getWriter().write(new Response("OK", "butter_layout", codeBuilder.toString()).getResponse());
 
         } catch (RequestException | JSONException | SAXException | ParserConfigurationException e) {
             e.printStackTrace();
             resp.getWriter().write(new Response(e.getMessage()).getResponse());
         }
 
+    }
+
+    private static String getFromFirstUpperChar(String string) {
+        final String[] chunks = string.split("(?=[A-Z])");
+        if (chunks.length > 1) {
+            return string.substring(chunks[0].length());
+        }
+        return string;
     }
 
 
---
+++ src/Lab.java
@@ -0,0 +1,10 @@
+/**
+ * Created by theapache64 on 18/12/17.
+ */
+public class Lab {
+    public static void main(String[] args) {
+
+    }
+
+
+}
